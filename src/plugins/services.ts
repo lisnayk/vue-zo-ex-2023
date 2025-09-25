823e0581057b7493d6f4d43f28c7a29c014675d7
Andrii Lisnyak <andrey.lisnyak@gmail.com>
2025-09-25 13:04:10 +0300

    Add initial Vue projects with Docker setup and Stepper component
    This commit introduces several Vue-related projects, including a Stepper component for navigation and a Docker configuration for running a Vue-lab project with Nginx and Certbot for SSL setup. It also includes initial project scaffolding, dependencies, and configurations for local development and production builds.

--- src/plugins/services.ts
+++ src/plugins/services.ts
@@ -2,7 +2,6 @@ import { provider } from '@/services'
 export default {
     install: (app) => {
         const services = provider();
-        console.log(services);
         app.config.globalProperties.$services = services;
         app.provide('services', services);
     }
